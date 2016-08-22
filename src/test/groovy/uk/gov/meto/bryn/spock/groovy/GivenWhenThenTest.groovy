52d4eeb628c46ff5a7816d6075635425265da2b6
bryn2008 <brymla.lloyd@gmail.com>
2016-08-22 16:51:13 +0100

    added a new unit test

---
+++ src/test/groovy/uk/gov/meto/bryn/spock/groovy/GivenWhenThenTest.groovy
@@ -0,0 +1,16 @@
+/**
+ * GivenWhenThenTest.groovy
+ *
+ * (C) Crown Copyright 2016, the Met Office.
+ */
+package uk.gov.meto.bryn.spock.groovy
+
+import spock.lang.Specification
+
+/**
+ * @author bryn.lloyd
+ *
+ */
+class GivenWhenThenTest extends Specification {
+
+}
--- src/test/groovy/uk/gov/meto/bryn/spock/groovy/SpockExpectWhereTest.groovy
+++ src/test/groovy/uk/gov/meto/bryn/spock/groovy/SpockExpectWhereTest.groovy
@@ -5,21 +5,32 @@
  */
 package uk.gov.meto.bryn.spock.groovy
 
-import org.spockframework.compiler.model.Spec
+import spock.lang.Specification
 
 /**
  * @author bryn.lloyd
  *
  */
-class SpockExpectWhereTest extends Spec {
+class SpockExpectWhereTest extends Specification {
 
     def 'should select a max of two numbers'(){
         expect:
         Math.max(a, b) == c
 
-        where:
-        a | b | c // these are vars that will be available in the above test
-        5 | 1 | 5 // initialised to these values!
-        9 | 9 | 9
+        /*where:
+         a | b | c // these are vars that will be available in the above test
+         5 | 1 | 5 // initialised to these values!
+         9 | 9 | 9*/
+
+        /*        where:
+         a << [5, 9]
+         b << [1, 9]
+         c << [5, 9]*/
+
+        //def sql = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
+        //where:
+        //[a, b, c]<< sql.rows("select a, b, c from mytable")
+        // or even:
+        //[a, b, _, c]<< sql.rows("select * from mytable")
     }
 }
